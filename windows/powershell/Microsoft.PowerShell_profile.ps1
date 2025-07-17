d17db8f5ea783d31c91f6aa53f53b208ce1e914a
xblackicex <xblackicex@outlook.com>
2025-07-17 20:18:03 +0200

    ✨ feat: add  win terminal and pwsh configs

---
+++ windows/powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,5 @@
+Invoke-Expression (& { (zoxide init powershell | Out-String) })
+Invoke-Expression (&starship init powershell)
+
+# replace 'Ctrl+t' and 'Ctrl+r' with your preferred bindings:
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
