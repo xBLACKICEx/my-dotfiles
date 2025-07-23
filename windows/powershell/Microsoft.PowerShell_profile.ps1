8a7b33479cccb6885ac10ea54d66df5eb83bd9fe
= <=>
2025-07-23 18:43:34 +0200

    ✨ feat: update PowerShell profile and add Atuin configuration

--- windows/powershell/Microsoft.PowerShell_profile.ps1
+++ windows/powershell/Microsoft.PowerShell_profile.ps1
@@ -2,4 +2,6 @@ Invoke-Expression (& { (zoxide init powershell | Out-String) })
 Invoke-Expression (&starship init powershell)
 
 # replace 'Ctrl+t' and 'Ctrl+r' with your preferred bindings:
-Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t'
+
+atuin init powershell | Out-String | Invoke-Expression
